9a7f45c9779ac9e5c36819dfb40b68d6fba028c7
Connor Cordell <clcordell93@gmail.com>
2021-06-07 17:00:22 +0100

    HTTP Server code - first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module dhsspares/http_server
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
+)
---
+++ server.go
@@ -0,0 +1,76 @@
+package http_server
+
+import (
+"crypto/tls"
+"github.com/gorilla/mux"
+"golang.org/x/crypto/acme/autocert"
+"net/http"
+"strings"
+"time"
+)
+
+type server struct {
+	production  bool
+	Server      *http.Server
+	Handler     *mux.Router
+	CertManager *autocert.Manager
+}
+
+var s server
+
+func init() {
+	s.CertManager = &autocert.Manager{
+		Prompt:          autocert.AcceptTOS,
+		Cache:           autocert.DirCache("certs"),
+	}
+	s.Handler = mux.NewRouter()
+	s.Server = &http.Server{
+		Addr:    ":443",
+		Handler: http.TimeoutHandler(caselessMatcher(s.Handler), time.Second*5, ""),
+		TLSConfig:         &tls.Config{
+			PreferServerCipherSuites: true,
+			MinVersion:               tls.VersionTLS12,
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			},
+		},
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       120 * time.Second,
+		MaxHeaderBytes:    http.DefaultMaxHeaderBytes,
+	}
+}
+
+func New(production bool) *server {
+	s.production = production
+	return &s
+}
+
+func (s *server) ListenAndServe() error {
+	return http.ListenAndServe(":80", s.CertManager.HTTPHandler(s.Handler))
+}
+
+func (s *server) ListenAndServeTLS() error {
+	var certFile, keyFile string
+	if s.production {
+		s.Server.Handler = s.CertManager.HTTPHandler(s.Server.Handler)
+	} else {
+		certFile = "_certs/localhost.pem"
+		keyFile = "_certs/localhost-key.pem"
+	}
+	return s.Server.ListenAndServeTLS(certFile, keyFile)
+}
+
+func caselessMatcher(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Path = strings.ToLower(r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
